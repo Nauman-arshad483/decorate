97f2207b626177efdecc0f6561ff6ee067d6d669
Nauman-arshad483 <nauman.arshad483@gmail.com>
2023-04-26 23:57:59 +0500

    final version

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 require('dotenv').config();
+const express = require('express');
 const http = require('http');
-const app = require('./app');
+const path = require('path');
 const logger = require('./utils/logger');
+const app = express();
 const server = http.createServer(app);
 const port = process.env.PORT || 5000;
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'frontend/build')));
+
+// Serve the React app
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
+});
+
+// Import your routes and middleware here
+app.use('/api', require('./routes/api'));
 
 server.listen(port, () => {
   logger.info(`Server running on port ${port}`);
