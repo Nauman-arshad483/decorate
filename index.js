68a203c04c926a8776c962de9e46bd67c63fee77
Nauman-arshad483 <nauman.arshad483@gmail.com>
2023-04-27 13:25:26 +0500

    re-build

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 app.use(express.static(path.join(__dirname, 'frontend/build')));
 
 // Serve the React app
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
 });
 
